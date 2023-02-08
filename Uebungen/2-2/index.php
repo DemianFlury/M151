b0f6e2142aac8fd04b2f3b608c09918c5d80aa07
Demian Flury <74318556+DemianFlury@users.noreply.github.com>
2023-02-08 15:03:18 +0100

    ex. 2-2

--- Uebungen/2-2/index.php
+++ Uebungen/2-2/index.php
@@ -7,7 +7,7 @@ $database = "northwind";
 try {
   $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password); //setzt den Typ der Datenbank fest.
 
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //setzt die Attribute des PDOs 
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //setzt die Attribute des PDOs fürs Verhalten bei Fehlern
   echo "Connected successfully";
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
