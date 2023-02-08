e0aaaac2bce441f18bcde0b0192bd0c63dea2bc1
Demian Flury <74318556+DemianFlury@users.noreply.github.com>
2023-02-08 14:25:37 +0100

    ex. 2-2

---
+++ Uebungen/2-2/index.php
@@ -0,0 +1,32 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "northwind";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password); //setzt den Typ der Datenbank fest.
+
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //setzt die Attribute des PDOs 
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+
+$sql = "SELECT * FROM customers";
+?>
+
+<table>
+    <?php
+    foreach($conn->query($sql) as $row){
+
+        echo "<tr>";
+            echo "<td>{$row['last_name']}</td>";
+            echo "<td>{$row['first_name']}</td>";
+            echo "<td>{$row['job_title']}</td>";
+            echo "<td>{$row['address']}</td>";
+            echo "<td>{$row['city']}</td>";
+        echo "</tr>";
+    }
+    ?>
+</table>
